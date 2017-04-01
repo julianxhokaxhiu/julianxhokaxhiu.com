891dfdc7598123281707f2d22a6412044e3cd80d
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-04-01 12:31:19 +0200

    First commit

---
+++ tpl/js/99_app.js
@@ -0,0 +1,81 @@
+(function($, viewport){
+  $(function(){
+    var $window = $(window),
+        $html = $('html'),
+        $body = $('body'),
+        $sections = $('.jx-section');
+        $sideBar = $('.jx-sidebar'),
+        $portfolioLinks = $('.jx-portfolio a');
+
+    $body
+    .on( 'click', 'a', function (e){
+      var $this = $(this),
+          href = $this.attr('href');
+
+      if ( href.indexOf( window.location.hostname ) == -1 ) {
+        e.preventDefault();
+        window.open( href );
+      }
+    });
+
+    $portfolioLinks
+    .on( 'click', function (e){
+      var $this = $(this),
+          $modal = $this.siblings('.modal');
+
+      if ( $modal.length ) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+
+      $modal
+      .modal( 'show' );
+    });
+
+    // Activate Tooltips
+    $('[data-toggle="tooltip"]')
+    .tooltip();
+
+    // Activate Scrollspy
+    $body
+    .scrollspy({
+      'target' : '#jx-scrollspy-target'
+    });
+
+    // Sidenav affixing
+    $sideBar
+    .affix({
+      offset: {
+        top: function () {
+          return $sideBar.offset().top
+        }
+      }
+    })
+    .find( 'ul li a[href^="#"]' )
+    .on( 'click', function (e){
+       // prevent default anchor click behavior
+       e.preventDefault();
+       e.stopPropagation();
+
+       // store hash
+       var hash = this.hash;
+
+       // animate
+       $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 300, function(){
+
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = hash;
+         });
+
+    });
+
+    // Rewrite history everytime scrollspy detect a change
+    $window
+    .on('activate.bs.scrollspy', function (e) {
+      history.replaceState( {}, '', $( 'a[href^="#"]', e.target ).attr("href") );
+    });
+  });
+})(jQuery, ResponsiveBootstrapToolkit);
